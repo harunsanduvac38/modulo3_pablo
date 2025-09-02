81c6cfdfe80d95f85f0365488556a85687e2f7da
harunsanduvac38 <harunsanduvac@gmail.com>
2025-09-02 09:28:54 +0200

    Add files via upload

---
+++ j01_introduccion/src/introduccion/MiSegundoPrograma.java
@@ -0,0 +1,9 @@
+package introduccion;
+
+public class MiSegundoPrograma {
+	public static void main(String[] arg) {
+		System.out.println("Linea 1");
+		System.out.print("Linea 2");
+		System.out.println("Linea 3");
+	}
+}
